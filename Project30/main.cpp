c858ed1aed90f88f049c5a8b01a2a15ae0d1c366
ordta11 <ordta11@29-6>
2025-12-05 11:53:56 +0200

    Add project files.

---
+++ Project30/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Payment
+{
+	protected:
+		string surname;
+		string name;
+		string patronymic;
+		int salary;
+		int yearOfEmployment;
+		double surchargeRate;//процент надбавки
+		double campingTax; //походный налог
+		int dayOfWorks;
+		int dayDo;
+		int creditingOfTheAmount;//начисление суммы 
+		int withholdingOfAnamount; //удержание суммы 
+public:
+	Payment() { salary = 0; yearOfEmployment = 0; surchargeRate = 0.0; campingTax = 0.0;  dayOfWorks = 0; dayDo = 0; creditingOfTheAmount = 0; withholdingOfAnamount = 0;}
+	Payment(string s, string n, string p, int sal, int y, double sur, double cm, int dw, int dd, int ca, int wa)
+	{
+	 surname = s;
+	 name = n;
+     patronymic = p;
+	 salary = sal;
+     yearOfEmployment = y;
+	 surchargeRate = sur;
+	 campingTax = cm;
+	 dayOfWorks = dw;
+	 dayDo = dd;
+     creditingOfTheAmount = ca;
+     withholdingOfAnamount = wa;
+	}
+
+	void Input(string s, string n, string p, int sal, int y, double sur, double cm, int dw, int dd, int ca, int wa)
+	{
+		surname = s;
+		name = n;
+		patronymic = p;
+		salary = sal;
+		yearOfEmployment = y;
+		surchargeRate = sur;
+		campingTax = cm;
+		dayOfWorks = dw;
+		dayDo = dd;
+		creditingOfTheAmount = ca;
+		withholdingOfAnamount = wa;
+	}
+
+
+
+	void Print()
+	{
+		cout << "surname: " << surname << "\tname: " << name << "\tpatronymic: " << patronymic << "\tsalary: " << salary << "\tyearOfEmployment: " << yearOfEmployment <<"\tsurchargeRate: " <<
+		surchargeRate << "\tcampingTax: " << "\tdayOfWorks: " << dayOfWorks << "\tdayDo: " << dayDo << "\tcreditingOfTheAmount: " << creditingOfTheAmount << "\twithholdingOfAnamount: " << withholdingOfAnamount;
+	}
+
+};
+
+
+int main()
+{
+
+}
